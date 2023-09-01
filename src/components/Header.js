3fed85ed16de96a355696c5ddb1871df003a1ee2
LaiaRuizM <laia.ruizm@hotmail.com>
2023-09-01 09:49:37 +0200

    feat: update Header structure and add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import "../styles/Header.scss";
+import logo from "../imagesVideos/biteMeLogo.jpeg";
 const Header = () => {
   return (
     <header className="header">
+      <img className="header--logo" src={logo} alt={logo}></img>
       <h2 className="header--title">Bite me in Welkhomeclub</h2>
       <h3 className="header--subtitle">Do you want to taste and bite it?</h3>
       <label className="header--searcher" htmlFor="search">
-        <input className="header--searcher__input" type="text" placeholder="" />
+        <i className="header__finder--icon fa-solid fa-magnifying-glass"></i>
+        <input
+          className="header--searcher__input"
+          type="text"
+          placeholder="What are going to taste today? Let's see!"
+          value={""}
+        />
       </label>
     </header>
   );
